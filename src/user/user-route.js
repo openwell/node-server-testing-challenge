ee164b2f0ba385251ccb1621c6f092d1a9d2858f
openwell <timileyinojo@yahoo.com>
2019-07-26 16:52:20 +0100

    create user routes

--- src/user/user-route.js
+++ src/user/user-route.js
@@ -1,12 +1,29 @@
 const db = require("./user-model");
 
 module.exports = server => {
-   server.get("/api/user", register);
+  server.get("/api/users", getUser);
+  server.post("/api/users", addUser);
 };
-
-function register(req, res) {
-  res.status(200).send('WElcome')
-
+const database = [];
+function getUser(req, res) {
+  res.status(200).send(database);
 }
 
-
+function addUser(req, res) {
+  const { name, hobby } = req.body;
+  if (!req.body) {
+    return res.status(400).json({
+      message: "missing user data"
+    });
+  } else if (!hobby || hobby.trim().length < 1) {
+    return res.status(400).json({
+      message: "missing required hobby field"
+    });
+  } else if (!name || name.trim().length < 1) {
+    return res.status(400).json({
+      message: "missing required name field"
+    });
+  }
+  database.push(req.body);
+  return res.status(200).json(req.body);
+}
